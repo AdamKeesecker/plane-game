d14eeadd3eff2007a8bde5f7e3a8e95c63208913
Adam Keesecker <adamkeesecker@gmail.com>
2015-01-28 11:09:20 -0600

    dependencies loaded

---
+++ app/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var dbname = process.env.DBNAME || 'default-plane-db';
+var port = process.env.PORT || 4000;
+var bodyParser     = require('body-parser');
+var express        = require('express');
+
+/* --- configuration    */
+var app = express();
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+/* --- pipeline          */
+app.use(express.static(__dirname + '/static'));
+app.use(bodyParser());
+
+/* --- http server       */
+var server = require('http').createServer(app);
+server.listen(port, function(){
+  console.log('Node server listening. Port: ' + port + ', Database: ' + dbname);
+});
+
+module.exports = app;
